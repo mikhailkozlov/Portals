22d2df2393e3abbcfe2fc9ad06a22a0093f4eb53
Mark K <mkrainij@sugarcrm.com>
2014-09-18 16:32:48 +0300

    FilesController: user, downloads updates

--- src/Sugarcrm/Portals/Controllers/Admin/FilesController.php
+++ src/Sugarcrm/Portals/Controllers/Admin/FilesController.php
@@ -15,20 +15,22 @@ use Illuminate\Support\MessageBag,
 class FilesController extends BaseController
 {
     protected $auth;
+    public $user;
     protected $file;
     protected $validator;
 
     public function __construct(\Sugarcrm\Portals\Repo\File $file, \Cartalyst\Sentry\Sentry $auth)
     {
-        $app             = app();
-        $this->auth      = $auth;
+        $app = app();
+        //$this->auth      = $auth;
+        $this->auth      = App::make('sentry'); // TODO: allow to see Sentry
+        $this->user      = $this->auth->getUser();
         $this->file      = $file;
         $this->validator = new FileValidator($app['validator'], new MessageBag);
-        //$this->filemanager = App::make('flysystem');
         parent::__construct();
     }
 
-    protected function getUserGroups()
+    protected function userGroupsList()
     {
         $userGroups = array();
         $addGroups  = $this->auth->getGroupProvider()->findAll();
@@ -60,7 +62,7 @@ class FilesController extends BaseController
      */
     public function create()
     {
-        $userGroups = $this->getUserGroups();
+        $userGroups = $this->userGroupsList();
 
         $this->layout->content = View::make(
             Config::get('portals::files.admin.create', 'portals::admin.files.create'),
@@ -94,7 +96,7 @@ class FilesController extends BaseController
         $input['extension'] = $input_file->getClientOriginalExtension();
         $input['type']      = $input_file->getMimeType();
         $input['size']      = $input_file->getSize();
-        //$input['user_id']   = $this->auth->getUser()->getId(); // TODO: does not see getId()
+        $input['user_id']   = $this->user->getId();
 
         $file = $this->file->create($input);
 
@@ -107,14 +109,12 @@ class FilesController extends BaseController
      * Show the form for editing the specified resource.
      *
      * @param  int $id
-     *
      * @return Response
      */
     public function edit($id)
     {
-        $file = $this->file->find($id);
-
-        $userGroups = $this->getUserGroups();
+        $file       = $this->file->find($id);
+        $userGroups = $this->userGroupsList();
 
         $this->layout->content = View::make(
             Config::get('portals::files.admin.edit', 'portals::admin.files.edit'),
@@ -123,33 +123,9 @@ class FilesController extends BaseController
     }
 
     /**
-     * Download file from Storage
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function download($id)
-    {
-        $file = $this->file->find($id);
-
-        if (is_null($file)) {
-            return Redirect::route('admin.files.view')->with('error', 'File not found.');
-        }
-
-        $tmpfname = $this->file->fmReadStream($file);
-
-        // save download
-        $file->increment('downloads');
-
-        return Response::download($tmpfname, $file->filename);
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  int $id
-     *
      * @return Response
      */
     public function update($id)
@@ -168,4 +144,29 @@ class FilesController extends BaseController
             "File information '{$input['title']}' has been saved"
         );
     }
+
+    /**
+     * Download file from Storage
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function download($id)
+    {
+        $file = $this->file->find($id);
+
+        if (is_null($file)) {
+            return Redirect::route('admin.files.view')->with('error', 'File not found.');
+        } elseif (!$this->user->inGroup($this->auth->findGroupById($file->permissions))) {
+            return Redirect::route('admin.files.view')->with('error', 'You have no access for this file.');
+        }
+
+        $tmpfname = $this->file->fmReadStream($file);
+
+        // save download
+        $file->increment('downloads'); // TODO:  ALTER TABLE `files` ADD COLUMN `downloads` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `size`;
+
+        return Response::download($tmpfname, $file->filename);
+    }
+
 }
